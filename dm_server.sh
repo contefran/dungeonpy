bd4d13dea63e99265430bc699a8e6f007bf4de35
Francesco <contefrances@gmail.com>
2026-04-19 15:33:38 +0200

    Discarded the old .sh connectors

--- dm_server.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Launch DungeonPy in DM mode (map + tracker + server for players).
-# Run this script to host a session. You will be prompted for a password.
-cd "$(dirname "$0")"
-python3 run_dnd_py.py --mode dm "$@"
